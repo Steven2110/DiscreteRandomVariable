27a928f365127e1cb2101cd5c46f4c17e46e55e5
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-09 23:53:47 +0700

    Initial Commit

---
+++ DiscreteRandomVariable/DiscreteRandomVariableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscreteRandomVariableApp.swift
+//  DiscreteRandomVariable
+//
+//  Created by Steven Wijaya on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscreteRandomVariableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
