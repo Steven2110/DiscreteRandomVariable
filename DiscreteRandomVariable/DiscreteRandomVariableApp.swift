043230d07d09db189ed47fa621cf7b55004fc9a8
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-05-10 03:06:31 +0700

    Setup gitignore, readme and rename ContentView

--- DiscreteRandomVariable/DiscreteRandomVariableApp.swift
+++ DiscreteRandomVariable/DiscreteRandomVariableApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DiscreteRandomVariableApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DRVView()
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Discrete Random Variable
+This is project for Simulation laboratory assignment № 9.
+In this project we are simulating a discrete random variable event with base generator.
